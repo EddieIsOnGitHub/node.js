8eebc2244fb42abaeb52dfe6e956b0061f4622b9
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-04-16 13:19:38 +0200

    new files

--- My_source_code/unit_1/lesson_7/main.js
+++ My_source_code/unit_1/lesson_7/main.js
@@ -1,40 +1,41 @@
-// const port = 3000,
-  // http = require("http"),
-  // httpStatus = require("http-status-codes"),
-  // router = require("./router"),
-  // contentTypes = require("./contentTypes"),
-//   utils = require("./utils"); http = require("http"),
-//     httpStatusCodes = require("http-status-codes"),
-//     router = require("./router"),
-//     fs = require("fs"),
-//     plainTextContentType = {
-//       "Content-Type": "text/plain"
-//     },
-//     htmlContentType = {
-//       "Content-Type": "text/html"
-//     },
-//     customReadFile = (file, res) => {
-//       fs.readFile(`./${file}`, (errors, data) => {
-//         if (errors) {
-//           console.log("Error reading the file...");
-//         }
-//         res.end(data);
-//       });
-//     };
-// router.get("/", (req, res) => {
-//   res.writeHead(httpStatusCodes.OK, plainTextContentType);
-//   res.end("INDEX");
-// });
-// router.get("/index.html", (req, res) => {
-//   res.writeHead(httpStatusCodes.OK, htmlContentType);
-//   customReadFile("views/index.html", res);
-// });
-// router.post("/", (req, res) => {
-//   res.writeHead(httpStatusCodes.OK, plainTextContentType);
-//   res.end("POSTED");
-// });
-// http.createServer(router.handle).listen(3000);
-// console.log(`The server is listening on port number: ${port}`);
+const port = 3000,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  router = require("./router"),
+  contentTypes = require("./contentTypes"),
+
+  utils = require("./utils"); http = require("http"),
+    httpStatusCodes = require("http-status-codes"),
+    router = require("./router"),
+    fs = require("fs"),
+    plainTextContentType = {
+      "Content-Type": "text/plain"
+    },
+    htmlContentType = {
+      "Content-Type": "text/html"
+    },
+    customReadFile = (file, res) => {
+      fs.readFile(`./${file}`, (errors, data) => {
+        if (errors) {
+          console.log("Error reading the file...");
+        }
+        res.end(data);
+      });
+    };
+router.get("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, plainTextContentType);
+  res.end("INDEX");
+});
+router.get("/index.html", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, htmlContentType);
+  customReadFile("views/index.html", res);
+});
+router.post("/", (req, res) => {
+  res.writeHead(httpStatusCodes.OK, plainTextContentType);
+  res.end("POSTED");
+});
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number: ${port}`);
 
 
 const fs = require("fs"),
